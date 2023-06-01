ae0134109d9c97d0f0c15f2b679fd5530d18d3ac
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2023-06-01 19:22:49 +0700

    Department controller & userinfo controller

---
+++ API/DTOs/UserInforDTO/LeaveDayLeftDto.cs
@@ -0,0 +1,14 @@
+namespace API.DTOs.UserInforDTO
+{
+    public class LeaveDayLeftDto
+    {
+        public int LeaveDayLeftId { get; set; }
+
+        public int? StaffId { get; set; }
+
+        public int? LeaveTypeId { get; set; }
+
+        public int? LeaveDayLeft1 { get; set; }
+
+    }
+}
---
+++ API/DTOs/UserInforDTO/StaffSkillDto.cs
@@ -0,0 +1,6 @@
+namespace API.DTOs.UserInforDTO
+{
+    public class StaffSkillDto
+    {
+    }
+}
