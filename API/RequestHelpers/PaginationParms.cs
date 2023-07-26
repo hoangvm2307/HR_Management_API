a877bc7a1e18045a9ad91d84e5a462aa542d7b73
huylinhit <huylinhit@gmail.com>
2023-07-26 22:17:34 +0700

    Update

--- API/RequestHelpers/PaginationParms.cs
+++ API/RequestHelpers/PaginationParms.cs
@@ -5,7 +5,7 @@
         private const int MaxPageSize = 50;
 
         public int PageNumber { get; set; } = 1;
-        private int _pageSize = 6;
+        private int _pageSize = 30;
         public int PageSize
         {
             get => _pageSize;
